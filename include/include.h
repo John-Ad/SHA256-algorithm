a1d06ff0acf288e9e3913bfeaff4bb7fcf45b2a9
John-Ad <adriaanscorne@gmail.com>
2021-06-14 22:50:47 +0200

    fixed memory leak issues

---
+++ include/include.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <bitset>
+#include <vector>
+#include <cstdint>
+#include <cstdlib>
+#include <time.h>
+
+using namespace std;
